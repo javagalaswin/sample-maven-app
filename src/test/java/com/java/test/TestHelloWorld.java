d9ddbb0b41ec6b75edfbbf89dcfbf4f9faa24eb6
javagalaswin <javagalaswin28@gmail.com>
2021-03-16 15:51:21 +0530

    Adding junit testing

---
+++ src/test/java/com/java/test/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package com.java.test;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		
+		 System.out.println( "Hello World!" );
+
+	}
+
+}
